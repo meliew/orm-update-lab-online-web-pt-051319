afe114af0a29c793cada69b83dd3865d85719d6a
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-24 22:57:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,40 +33,40 @@ class Student
   end
 
   def save
-  if self.id
-    self.update
-  else
-    sql = <<-SQL
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
       INSERT INTO students (name, grade)
       VALUES (?, ?)
-    SQL
+      SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
 
   def update
 
-  sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
-  DB[:conn].execute(sql, self.name, self.grade, self.id)
-end
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
+  end
 
 
-def self.create(name, grade)
-  student = Student.new(name, grade)
-  student.save
-  student
-end
+  def self.create(name, grade)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
 
-def self.new_from_db(row)
-  id = row[0]
-  name = row[1]
-  grade = row[2]
-  self.new(id, name, grade)
-end
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end
 
-def self.find_by_name(name)
+  def self.find_by_name(name)
     sql = <<-SQL
     SELECT *
     FROM students
